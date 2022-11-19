71657aea05342d74567ccae0d183a8ca409463e8
Mark Lhester Agellon <kianirel56@gmail.com>
2022-11-19 17:09:45 +0800

    Username label is inserted

--- Login System Using Tkinter.py
+++ Login System Using Tkinter.py
@@ -10,13 +10,15 @@ root.resizable(False,False)
 img = PhotoImage(file="images/login.png")
 Label(root, image=img, bg="white").place(x=50, y=50)
 
-frame = Frame(root, width=350, height=350, bg="red")
+frame = Frame(root, width=350, height=350, bg="white")
 frame.place(x=480, y=70)
 
 heading = Label(frame, text="Sign in", fg="#57a1f8", bg="white", font=("Microsoft Yahei Light", 23, "bold"))
 heading.place(x=100, y=5)
 
-user = Entry(frame, width=25, fg="black", border=2, bg="white", font=("Microsoft Yahei Light",11))
+user = Entry(frame, width=25, fg="black", border=0, bg="white", font=("Microsoft Yahei Light",11))
 user.place(x=30, y=80)
+user.insert(0,"Username")
+
 
 root.mainloop()
