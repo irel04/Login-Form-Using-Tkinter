3e399aa5fc88aef844a1d23018504478687ee6d6
Mark Lhester Agellon <kianirel56@gmail.com>
2022-11-19 17:07:46 +0800

    user entry was implemented on the GUI

--- Login System Using Tkinter.py
+++ Login System Using Tkinter.py
@@ -16,6 +16,7 @@ frame.place(x=480, y=70)
 heading = Label(frame, text="Sign in", fg="#57a1f8", bg="white", font=("Microsoft Yahei Light", 23, "bold"))
 heading.place(x=100, y=5)
 
-user = Entry(frame, width=2)
+user = Entry(frame, width=25, fg="black", border=2, bg="white", font=("Microsoft Yahei Light",11))
+user.place(x=30, y=80)
 
 root.mainloop()
