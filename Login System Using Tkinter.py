9112ba7552a006048b2185cacddc2cc2c81fdcd4
Mark Lhester Agellon <kianirel56@gmail.com>
2022-11-19 17:02:36 +0800

    header for sign in wass declared on frame window

--- Login System Using Tkinter.py
+++ Login System Using Tkinter.py
@@ -12,4 +12,8 @@ Label(root, image=img, bg="white").place(x=50, y=50)
 
 frame = Frame(root, width=350, height=350, bg="red")
 frame.place(x=480, y=70)
+
+heading = Label(frame, text="Sign in", fg="#57a1f8", bg="white", font=("Microsoft Yahei Light", 23, "bold"))
+
+
 root.mainloop()
