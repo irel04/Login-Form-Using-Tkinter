cb55ca919a8115c578d5efed0621b42eb87b95b5
Mark Lhester Agellon <kianirel56@gmail.com>
2022-11-19 16:51:51 +0800

    TK() class was not defined instead Tk()

--- Login System Using Tkinter.py
+++ Login System Using Tkinter.py
@@ -1,7 +1,7 @@
 from tkinter import* 
 from tkinter import messagebox
 
-root=TK()
+root=Tk()
 root.title("Login")
 root.geometry("925x500+300+200")
 root.configure(bg="")
@@ -9,3 +9,5 @@ root.resizable(False,False)
 
 img = PhotoImage(file="images/login.png")
 Label(root, image=img, bg="white").place(x=50, y=50)
+
+root.mainloop()
